676b19ecfa0d98720d9acef4ab783093234797dc
Alex Isvoranu <alx.isvoranu@yahoo.com>
2026-07-10 16:04:14 +0300

    add health check endpoint

--- infra/main.tf
+++ infra/main.tf
@@ -201,12 +201,12 @@ resource "aws_lb_target_group" "ipark_tg" {
   target_type = "ip"
 
   health_check {
-    path                = "/iPark"
-    matcher             = "200-399"
-    healthy_threshold   = 3
-    unhealthy_threshold = 3
-    timeout             = 5
+    path                = "/iPark/health"
+    matcher             = "200"
     interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 3
   }
 }
 
---
+++ src/main/java/eu/ase/ro/parking/controller/HealthCheckController.java
@@ -0,0 +1,13 @@
+package eu.ase.ro.parking.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+    @GetMapping("/health")
+    public ResponseEntity<String> check() {
+        return ResponseEntity.ok("UP");
+    }
+}
