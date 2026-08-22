372b3a734a3af9596d96f921fd1ff5d26e2fe8f3
Alex Isvoranu <alx.isvoranu@yahoo.com>
2026-08-22 14:37:14 +0300

    feat(infra): manage k8s load balancer service via terraform and streamline ci-cd deployment

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,17 +1,3 @@
-resource "aws_secretsmanager_secret" "app_secrets" {
-  name                    = "ipark/${var.environment}/secrets"
-  recovery_window_in_days = 0
-}
-
-resource "aws_ecr_repository" "ipark_app" {
-  name                 = "${var.environment}-ipark-app"
-  image_tag_mutability = "MUTABLE"
-
-  image_scanning_configuration {
-    scan_on_push = true
-  }
-}
-
 module "vpc" {
   source      = "./modules/vpc"
   environment = var.environment
@@ -38,4 +24,55 @@ module "eks" {
   public_subnet_b_id      = module.vpc.public_subnet_b_id
   server_sg_id            = module.security.server_sg_id
   github_actions_role_arn = module.iam.github_actions_role_arn
+}
+
+resource "aws_secretsmanager_secret" "app_secrets" {
+  name                    = "ipark/${var.environment}/secrets"
+  recovery_window_in_days = 0
+}
+
+resource "aws_ecr_repository" "ipark_app" {
+  name                 = "${var.environment}-ipark-app"
+  image_tag_mutability = "MUTABLE"
+  force_delete         = true
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks.cluster_name
+}
+
+provider "kubernetes" {
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+}
+
+resource "kubernetes_service" "ipark_service" {
+  metadata {
+    name   = "ipark-service"
+    labels = {
+      app = "ipark-backend"
+    }
+  }
+
+  spec {
+    selector = {
+      app = "ipark-backend"
+    }
+
+    port {
+      port        = 80
+      target_port = 8080
+    }
+
+    type = "LoadBalancer"
+  }
+
+  depends_on = [
+    module.eks
+  ]
 }
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,3 +2,8 @@ output "ecr_repository_url" {
   value       = aws_ecr_repository.ipark_app.repository_url
   description = "The URL of the ECR repository"
 }
+
+output "service_status" {
+  description = "Status of the deployed Kubernetes Service"
+  value       = kubernetes_service.ipark_service.status
+}
